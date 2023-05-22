5c5c556f02c83a9607f7457a29749b5ce4f4b50a
TobiasUr <68461330+TobiasUr@users.noreply.github.com>
2023-05-22 19:28:10 +0200

    Now with html
    Can write flight into excel - not formated

--- Main.py
+++ Main.py
@@ -1,101 +1,34 @@
-import pyautogui
-import cv2
-import pytesseract
+from bs4 import BeautifulSoup
 import pandas as pd
-from PIL import Image
-import numpy as np
-import xlwt
-import csv
 import openpyxl
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Tobia\AppData\Local\Tesseract-OCR\tesseract.exe'
-
-img= cv2.imread("TEST.png")
-imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+#Create workbook object
 wb = openpyxl.Workbook()
-ws = wb.worksheets[0]
+sheet = wb.create_sheet()
+sheet.title='Flights'
 
 
+#open txt file
+filepath = "FlightMemory - FlightData.htm"
+file = open(filepath, 'r')
+data = file.read()
+#print(file.read())
 
+#remove first part'
+#index = data.find('Options')
+#data[index:]
 
-def ROI(img, vertices):
-    mask=np.zeros_like(img)
-    cv2.fillPoly(mask, vertices, 255)
-    masked = cv2.bitwise_and(img, mask)
-    print('hello')
-    return masked
-
-
-# Get the size
-w = img.shape[1]
-h = img.shape[0]
-resolution = np.array([w, h])
-
-def readText(cropStart: float, cropEnd: float, outputName:str):
-    vertices = np.array([[resolution[0]/cropStart, 0],[resolution[0]/cropEnd, 0], [resolution[0]/cropEnd, resolution[1]], [resolution[0]/cropStart, resolution[1]]], np.int32)
-    #imgCropped =ROI(imgGray, [vertices])
-
-    y1 = 0
-    y2 = int(resolution[1])
-    x1= int(resolution[0]/cropStart)
-    x2= int(resolution[0]/cropEnd)
-    imgCropped = imgGray[y1:y2, x1:x2]
-    flt = cv2.adaptiveThreshold(imgCropped,
-                            100, cv2.ADAPTIVE_THRESH_MEAN_C,
-                           cv2.THRESH_BINARY, 15, 16,
-                           )
-
-    scale_percent = 150 # percent of original size
-    width = int(imgCropped.shape[1] * scale_percent / 100)
-    height = int(imgCropped.shape[0] * scale_percent / 100)
-    dim = (width, height)
-      
-    flt = cv2.resize(flt, dim, interpolation = cv2.INTER_AREA)
-
+data = BeautifulSoup(data, 'html.parser')
+container = data.select_one('.container')
+tablebody = container.find_all('tbody')[2]
+trs = tablebody .find_all('tr', recursive=False)
+global i
+i = 1
+for idx, tr in enumerate(trs):
+    idx += 1
+    sheet.cell(row = idx, column = 1).value=tr.get_text()
+    print(idx)    
+    #print(tr.get_text())
+wb.save('Flights.xlsx')
     
-    
-    
-
-    flt = imgCropped
-
-    config = ('-l eng — oem 1 — -psm 6')
-
-    text = pytesseract.image_to_string(flt, config=config)
-
-    with open(outputName, 'w') as f:
-        f.write(text)
-
-    inputFile = outputName
-    outputFile = outputName.replace(".txt", ".csv")
-
-    with open(inputFile, 'r') as data:
-        reader = csv.reader(data, delimiter='\t')
-        for row in reader:
-            ws.append(row)
-            
-    wb.save(outputFile)
-
-    #cv2.imshow('window', flt)
-    #cv2.waitKey()
-
-
-
-
-
-
-
-
-readText(w, 35, '1.txt')
-readText(35, float(9.46), '2.txt')
-readText(9.46, 6.899, '3.txt')
-readText(6.899, 3.63, '4.txt')
-readText(3.63, 3.17, '5.txt')
-readText(3.17, 2.04, '6.txt')
-readText(2.04, 1.79, '7.txt')
-readText(1.79, 1.45, '8.txt')
-readText(1.45, 1.189, '9.txt')
-readText(1.189, 1.08, '10.txt')
-
-
 
