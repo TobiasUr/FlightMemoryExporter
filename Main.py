8948932dd8959291b9a4eeab861d8d796357af77
TobiasUr <68461330+TobiasUr@users.noreply.github.com>
2023-05-23 21:11:37 +0200

    Now logs in and downloads html for you

--- Main.py
+++ Main.py
@@ -1,34 +1,114 @@
 from bs4 import BeautifulSoup
 import pandas as pd
 import openpyxl
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+
+Chromdriver = 'chromedriver.exe'
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("start-maximized")
+chrome_options.add_argument("--disable-blink-features")
+chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+
+
+#login info
+LOGIN_PAGE = "https://www.flightmemory.com/"
+ACCOUNT = "*******"
+PASSWORD = "******"
+
+driver = webdriver.Chrome(executable_path=Chromdriver, chrome_options=chrome_options)
+
+
+#login
+driver.get(LOGIN_PAGE)
+wait = WebDriverWait(driver, 30)
+wait.until(EC.element_to_be_clickable((By.NAME, "username"))).send_keys(ACCOUNT)
+print('enteredName')
+wait.until(EC.element_to_be_clickable((By.NAME, "passwort"))).send_keys(PASSWORD)
+print('enteredPassword')
+wait.until(EC.element_to_be_clickable((By.XPATH, ".//input[@value='SignIn' and @type='submit']"))).click()
+print("Logged in")
+#go to flights
+
+wait.until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'FLIGHTDATA')]"))).click()
+print('found flightdata')
+pages = []
+pages.append(driver.execute_script("return document.documentElement.outerHTML"))
+
+
+while True:
+    try:
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//img[contains(@src,'/images/next.gif')]"))).click()
+        pages.append(driver.execute_script("return document.documentElement.outerHTML"))
+    except TimeoutException:
+        print("Pages Found:")
+        print(int(len(pages)))
+        break
+
 
 #Create workbook object
 wb = openpyxl.Workbook()
-sheet = wb.create_sheet()
+sheet = wb.get_sheet_names()[0]
+sheet = wb.get_sheet_by_name(sheet)
 sheet.title='Flights'
 
+#TableInfo
+sheet.cell(row = 1, column = 1).value='No.'
+sheet.cell(row = 1, column = 2).value='Date - Dep./Arr.'
+sheet.cell(row = 1, column = 3).value='Dep.'
+sheet.cell(row = 1, column = 4).value='Arr.'
+sheet.cell(row = 1, column = 5).value='Dis. [Km]'
+sheet.cell(row = 1, column = 6).value='Time'
+sheet.cell(row = 1, column = 7).value='Airline/FlightNumber'
+sheet.cell(row = 1, column = 8).value='Airplane'
+sheet.cell(row = 1, column = 9  ).value='Seat'
 
-#open txt file
-filepath = "FlightMemory - FlightData.htm"
-file = open(filepath, 'r')
-data = file.read()
-#print(file.read())
 
-#remove first part'
-#index = data.find('Options')
-#data[index:]
 
-data = BeautifulSoup(data, 'html.parser')
-container = data.select_one('.container')
-tablebody = container.find_all('tbody')[2]
-trs = tablebody .find_all('tr', recursive=False)
-global i
-i = 1
-for idx, tr in enumerate(trs):
-    idx += 1
-    sheet.cell(row = idx, column = 1).value=tr.get_text()
-    print(idx)    
-    #print(tr.get_text())
+global lastidx 
+lastidx = 0
+
+for page in pages:
+    #find table with BS
+    data = BeautifulSoup(page, 'html.parser')
+    container = data.select_one('.container')
+    tablebody = container.find_all('tbody')[2]
+    trs = tablebody.find_all('tr', recursive=False)
+
+    #loop through each line
+    for idx, tr in enumerate(trs):
+        idx += lastidx
+        idx += 1
+        
+        if idx != lastidx + 1:
+            #get info
+            flight_number = tr.find_all('td')[0]
+            date_dep_arr = tr.find_all('td')[1]
+            Departure = tr.find_all('td')[2]
+            Arrival = tr.find_all('td')[4]
+            Distance = tr.find_all('td')[6]
+            FlightTime =tr.find_all('td')[8]
+            Airline_Flightinfo = tr.find_all('td')[10]
+            Airplane = tr.find_all('td')[11]
+            Seat = tr.find_all('td')[12]
+            #addInfoToSheet
+            sheet.cell(row = idx, column = 1).value=flight_number.get_text()
+            sheet.cell(row = idx, column = 2).value=date_dep_arr.get_text()
+            sheet.cell(row = idx, column = 3).value=Departure.get_text()
+            sheet.cell(row = idx, column = 4).value=Arrival.get_text()
+            sheet.cell(row = idx, column = 5).value=Distance.get_text()
+            sheet.cell(row = idx, column = 6).value=FlightTime.get_text()
+            sheet.cell(row = idx, column = 7).value=Airline_Flightinfo.get_text()
+            sheet.cell(row = idx, column = 8).value=Airplane.get_text()
+            sheet.cell(row = idx, column = 9).value=Seat.get_text()
+        print(idx)    
+    lastidx = idx
+
 wb.save('Flights.xlsx')
-    
+        
 
